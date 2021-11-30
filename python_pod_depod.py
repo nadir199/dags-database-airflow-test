639f8a7f3c6f4f5481b71d0320bdf7afbd41c1b9
Nadir DEBBAGH <nadir199@live.fr>
2021-11-30 14:02:09 +0100

    Add test pod from file

---
+++ python_pod_depod.py
@@ -0,0 +1,54 @@
+from datetime import timedelta
+
+from airflow import DAG
+
+from airflow.utils.dates import days_ago
+
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+import os
+
+default_args = {
+	"owner" : "airflow",
+	"depends_on_past" : False,
+	"email" : ["airflow@example.com"],
+	"email_on_failure" : False,
+	"email_on_retry" : False,
+	"retries" : 1,
+	"retry_delay" : timedelta(minutes = 5),
+}
+
+with DAG(
+	"python_kubernetes_workflow_hourly",
+	default_args = default_args,
+	description="python_kubernetes_workflow_hourly",
+	schedule_interval=timedelta(minutes=60),
+	start_date=days_ago(0),
+	tags=["python_kubernetes_workflow_hourly"]
+) as dag:
+	t1 = KubernetesPodOperator(
+		namespace = "default",
+		pod_template_file="pod_test.yaml",
+		labels =  {"foo":"bar"},
+		name = "task-depod",
+		is_delete_operator_pod = True,
+		in_cluster = True,
+		task_id = "task-depod",
+		config_file=os.path.expanduser("~") + "/.kube.config",
+		get_logs = True
+	)
+
+	t2 = KubernetesPodOperator(
+		namespace = "default",
+		image = "python:3.7",
+		image_pull_policy = "Always",
+		cmds = ["python", "-c", "print('DEUXIEME TASK ..................')"],
+		labels =  {"foo":"bar"},
+		name = "task-2-h",
+		is_delete_operator_pod = True,
+		in_cluster = True,
+		task_id = "task-2-h",
+		config_file=os.path.expanduser("~") + "/.kube.config",
+		get_logs = True
+	)
+
+	t1 >> t2

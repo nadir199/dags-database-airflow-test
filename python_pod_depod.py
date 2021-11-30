166d481eceb20e86d317b50b3106201be6b77678
Nadir DEBBAGH <nadir199@live.fr>
2021-11-30 14:09:50 +0100

    change dag name

--- python_pod_depod.py
+++ python_pod_depod.py
@@ -18,12 +18,12 @@ default_args = {
 }
 
 with DAG(
-	"python_kubernetes_workflow_hourly",
+	"python_kubernetes_pod_depod",
 	default_args = default_args,
-	description="python_kubernetes_workflow_hourly",
+	description="python_kubernetes_pod_depod",
 	schedule_interval=timedelta(minutes=60),
 	start_date=days_ago(0),
-	tags=["python_kubernetes_workflow_hourly"]
+	tags=["test_pod"]
 ) as dag:
 	t1 = KubernetesPodOperator(
 		namespace = "default",
@@ -43,10 +43,10 @@ with DAG(
 		image_pull_policy = "Always",
 		cmds = ["python", "-c", "print('DEUXIEME TASK ..................')"],
 		labels =  {"foo":"bar"},
-		name = "task-2-h",
+		name = "task-2-hdep",
 		is_delete_operator_pod = True,
 		in_cluster = True,
-		task_id = "task-2-h",
+		task_id = "task-2-hdep",
 		config_file=os.path.expanduser("~") + "/.kube.config",
 		get_logs = True
 	)

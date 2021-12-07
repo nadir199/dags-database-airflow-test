29532a380a5c3c1ef349990b1531e250eb6af884
Nadir DEBBAGH <nadir199@live.fr>
2021-12-07 11:17:56 +0100

    2 tasks only

---
+++ exec_airflow.py
@@ -0,0 +1,51 @@
+from datetime import timedelta
+
+from airflow import DAG
+
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+import os
+
+print("STARTING DAG POD DEPOD CREATION")
+
+default_args = {
+	"owner" : "airflow",
+	"depends_on_past" : False,
+	"email" : ["airflow@example.com"],
+	"email_on_failure" : False,
+	"email_on_retry" : False,
+	"retries" : 1,
+	"retry_delay" : timedelta(minutes = 5),
+}
+
+with DAG(
+	"python_kubernetes_pod_depod",
+	default_args = default_args,
+	description="python_kubernetes_pod_depod",
+	schedule_interval=timedelta(minutes=60),
+	start_date=days_ago(0),
+	tags=["test_pod"]
+) as dag:
+	tt = BashOperator(
+		task_id = "dummy",
+		bash_command = "echo TESTDUMMY"
+	)
+
+	t1 = KubernetesPodOperator(
+		namespace = "default",
+		pod_template_file = "./pod_test.yaml",
+		labels = {"foo":"bar"},
+		name = "task-1-hdep",
+		is_delete_operator_pod = True,
+		in_cluster = True,
+		task_id = "task-1-hdep",
+		config_file = os.path.expanduser("~") + "/.kube.config",
+		get_logs = True
+
+	)
+	print("FINISHED CREATING TASK 1 POD TEST")
+
+	tt >> t1
+print("FINISHED CREATING DAG")
